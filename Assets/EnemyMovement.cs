9bc902615930a0b2f74f6909d7b74990de32636d
WoongGun <woonggun@jchoi.ca>
2015-01-25 02:28:00 -0800

    Added kill tracking cam for side attacks

--- Assets/EnemyMovement.cs
+++ Assets/EnemyMovement.cs
@@ -1,38 +1,63 @@
 ﻿using UnityEngine;
 using System.Collections;
+
+using System;
+using System.Threading;
  
 public class EnemyMovement : MonoBehaviour
 {
 	public float playerAttraction;
 	public float moveSpeed;
 	public float chaseDistance;
+	public float killDistance;
 	public float yHeight;
 	public float threatDistance;
 	public Animator anim; 
 	public float attackSpeed;
 
+	private Transform killCam;
 	private Transform player;               // Reference to the player's position.
 	private float mostFear = 0.0f;
 	private Collider mostFeared;  
-	private bool isAlive = true;
+	public bool isAlive = true;
+	public bool gameOver = false;
 
 	void Awake ()
 	{
 		// Set up the references.
 		player = GameObject.FindGameObjectWithTag ("Player").transform;
+		
+		killCam = GameObject.Find("KillCam").camera.transform;
 	}
 	void Update ()
 	{
-		if (DistFromPlayer() < threatDistance)
+		
+		playerLookAt(killCam);
+
+		if(DistFromPlayer() < killDistance)
+		{
+			if (gameOver == false){
+				isAlive = false;
+				gameOver = true;
+				Thread.Sleep(200);
+				BroadcastMessage("CharacterDeath", this.transform);
+			}
+		}
+
+		else if (DistFromPlayer() < threatDistance)
 		{
 			if (isAlive)
 			{
+				isAlive = false;
 				attackPlayer();
+				
+				BroadcastMessage("CharacterDeath", this.transform);
 			}
+
 			lookAt(player);
 			transform.position += transform.forward*attackSpeed*Time.deltaTime;
-
 		}
+
 		else if (mostFear >= playerAttraction) 
 		{
 			lookAt(mostFeared.transform);
@@ -40,6 +65,7 @@ public class EnemyMovement : MonoBehaviour
 			gameObject.transform.LookAt(player);
 			mostFear = 0.0f;
 		}
+
 		else if(DistFromPlayer() <= chaseDistance)
 		{
 			lookAt(player);
@@ -94,13 +120,21 @@ public class EnemyMovement : MonoBehaviour
 		anim = GetComponent <Animator> ();
 		anim.SetTrigger ("Dead");
 		threatDistance = 100.0f;
-		isAlive = false;
 	}
+
 	void lookAt(Transform other)
 	{
 		Vector3 otherPos = other.position;
 		otherPos.y = yHeight;
 		gameObject.transform.LookAt(otherPos);
 	}
+
+	void playerLookAt(Transform killCam)
+	{
+		Vector3 killerPos = transform.position;
+		killerPos.y = yHeight + 1;
+		killCam.transform.LookAt(killerPos);
+	}
+	
 }
 
